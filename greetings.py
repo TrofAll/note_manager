0c4585502c5ae0fdd81d81a5845dfa55928cda8a
TrofAll <trofall7@gmail.com>
2025-01-06 14:59:43 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,7 @@
+username = 'alex'
+title = 'car'
+content = 'maintenance'
+status = 'important'
+created_date = "06-01-2025"
+issue_date = "06-02-2026"
+print (username, title, content, status, created_date, issue_date)
