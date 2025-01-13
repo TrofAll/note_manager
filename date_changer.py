99ecf2c94394d584bf23d503fc057bc405ce36df
TrofAll <trofall7@gmail.com>
2025-01-13 12:36:20 +0300

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,12 @@
+username = 'alex'
+title = 'car'
+content = 'maintenance'
+status = 'important'
+created_date = "06-01-2025"
+issue_date = "06-02-2026"
+print(username)
+print(title)
+print(content)
+print(status)
+print(created_date [0:5])
+print(issue_date [0:5])
