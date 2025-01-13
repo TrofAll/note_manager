e493a9beaeee7097ea88d15081d2c63ab3eb183b
TrofAll <trofall7@gmail.com>
2025-01-13 22:03:50 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = 'Алекандр'
+title = 'Машина'
+content = 'техобслуживание'
+status = 'важно'
+created_date = "10-01-2025"
+issue_date = "10-02-2025"
+print('Имя ползователя:',username)
+print('Заголовок заметки:',title)
+print('Описание заметки:',content)
+print('Статус заметки:',status)
+print('Дата начала:',created_date)
+print('Дата завершения:',issue_date)
