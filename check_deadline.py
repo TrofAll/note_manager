2cda4dae970fd305fbaca7ae8da5f7be8fc60f50
TrofAll <trofall7@gmail.com>
2025-01-20 21:36:04 +0300

    Add files via upload

---
+++ check_deadline.py
@@ -0,0 +1,33 @@
+from datetime import datetime # Импортируем модуль из библиотеки
+
+
+current_datetime = datetime.now() # Получаем текущую дату
+
+formatted_datetime = current_datetime.strftime("%d-%m-%Y") # Преобразовываем дату в нужный нам формат
+
+current_date = datetime.strptime(formatted_datetime, "%d-%m-%Y") # Преобразовываем строку
+
+print(f"Текущая дата: {formatted_datetime}") #Выводим текущу дату на консоль
+
+while True: # Начало цикла, будет продолжаться, пока ПЗ не введет корректную дату, в случае ошибки, повторяется
+    try: # Запускаем блок проверки ошибок
+        issue_date = input("Введите дату дедлайна (в формате день-месяц-год, например 20-01-2025): " ) # Просим ввести дату
+        # в правильном формате
+
+        issue_date_ = datetime.strptime(issue_date,"%d-%m-%Y") # Преобразуем дату(строку) в объект datetime,
+        # необходимо для вычеслений
+
+        time_difference = issue_date_ - current_date # Вычесляем раницу между датой дедлайна и текущей датой
+        days_difference = time_difference.days # Извлекаем разницу в днях
+
+        if days_difference > 0: # Проверка разница и вывод в консоль соотвествующего значения
+            print(f"До делайна осталось {days_difference} дней!")
+        elif days_difference < 0:
+            print(f"Дедлайн истек {abs(days_difference)} дней назад!")
+        else:
+            print("Дедлайн сегодня!")
+
+        break # Выходим из цикла при выполнение условий
+
+    except ValueError: # Обработка ошибки неверного формата даты
+        print("Неверный формат даты! Пожалуйста, введи правильный формат даты, например 20-01-2025!")
